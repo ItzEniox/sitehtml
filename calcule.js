7464638835a58b0de5399a1ad00231593674dfad
Karl2301 <karl.rahuel@gmail.com>
2024-05-19 18:58:42 +0200

    update

--- calcule.js
+++
@@ -1,5 +0,0 @@
-//TR = Temps de course en minute    FL = Carburant par tour     TL = Temps par tour
-var imola_acc = (TR / TL) * FL + (2 * FL);
-const FL = 2.9;
-const TL = 1.72;
-const TR = "Utilisateur choisi";
